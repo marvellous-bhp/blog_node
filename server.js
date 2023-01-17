4081b42335031459c33917c2730c09cc2509810d
haiduongdana <haiduongdana@gmail.com>
2023-01-17 15:35:18 +0700

    blog ver 1 17/1

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+const path = require('path')
+const port = 3433
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+//view
+app.set('views/articles',path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+//app conf
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(port, () => {
+  console.log(`port ${port}`);
+});
