32f67ac063dcd7de8dc0f2bcf29c9539f3f94f03
marvellous-bhp <haiduongdana@gmail.com>
2023-01-17 15:40:57 +0700

    blog 17/1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Article = require('./models/article')
+const Article = require('./src/models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
